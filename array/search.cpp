c9b3b76b7c25069a7ef39192fb8007ce1fe91d5b
Gesimo Gapitano <gesimogapitano@gmail.com>
2026-05-12 09:27:08 +0300

    Refactor code structure for improved readability and maintainability

---
+++ array/search.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            return i; // return index if found
+        }
+    }
+    return -1; // not found
+}
+
+int main() {
+    int n, key;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int result = linearSearch(arr, n, key);
+
+    if (result != -1) {
+        cout << "Element found at index " << result << endl;
+    } else {
+        cout << "Element not found in array" << endl;
+    }
+
+    return 0;
+}
